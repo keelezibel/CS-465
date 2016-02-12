33a94bd556f2562499c5b2b3d2e1fba4e294f55e
Le Tang Boon <letang.boon@gmail.com>
2016-02-12 11:04:03 -0600

    New test project

---
+++ CS465/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CS465
+//
+//  Created by Le Tang Boon on 2/12/16.
+//  Copyright © 2016 Le Tang Boon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
