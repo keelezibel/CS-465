8ec6ee4d85c459721ed869e07945f765c9b035be
Le Tang <letang.boon@gmail.com>
2016-03-26 18:20:43 -0500

    new files

---
+++ Quit Smoking/Quit Smoking/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Quit Smoking
+//
+//  Created by Le Tang Boon on 3/26/16.
+//  Copyright © 2016 Le Tang Boon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: ResponsiveTextFieldViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
+
