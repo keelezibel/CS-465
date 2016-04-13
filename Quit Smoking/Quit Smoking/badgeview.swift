2174a9af1a1dbfc79527dd369e496a0682030aca
suree404 <sureesuree404@gmail.com>
2016-04-12 21:45:19 -0500

    badge page not finished

---
+++ Quit Smoking/Quit Smoking/badgeview.swift
@@ -0,0 +1,12 @@
+//
+//  badgeview.swift
+//  Quit Smoking
+//
+//  Created by Sushan Jiang on 4/12/16.
+//  Copyright © 2016 Le Tang Boon. All rights reserved.
+//
+import UIKit
+class badgeview: ResponsiveTextFieldViewController{
+
+
+}
