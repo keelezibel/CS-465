a8e320508f8a967c4f023baba88c6352bb2f40e5
Le Tang <letang.boon@gmail.com>
2016-04-12 21:32:56 -0500

    Add dropdown

--- Quit Smoking/Quit Smoking/ViewController.swift
+++ Quit Smoking/Quit Smoking/ViewController.swift
@@ -42,3 +42,36 @@ class ViewController: ResponsiveTextFieldViewController{
 
 }
 
+class MainUI:UIViewController{
+    
+    var menuView: BTNavigationDropdownMenu!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let items = ["Settings", "Daily Reminder", "Modify Profile", "Change Quit 4 Money Plan", "Product Info"]
+        self.navigationController?.navigationBar.translucent = false
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 0.0/255.0, green:180/255.0, blue:220/255.0, alpha: 1.0)
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+        
+        menuView = BTNavigationDropdownMenu(navigationController: self.navigationController, title: items.first!, items: items)
+        menuView.cellHeight = 50
+        menuView.cellBackgroundColor = self.navigationController?.navigationBar.barTintColor
+        menuView.cellSelectionColor = UIColor(red: 0.0/255.0, green:160.0/255.0, blue:195.0/255.0, alpha: 1.0)
+        menuView.cellTextLabelColor = UIColor.whiteColor()
+        menuView.cellTextLabelFont = UIFont(name: "Avenir-Heavy", size: 17)
+        menuView.cellTextLabelAlignment = .Left // .Center // .Right // .Left
+        menuView.arrowPadding = 15
+        menuView.animationDuration = 0.5
+        menuView.maskBackgroundColor = UIColor.blackColor()
+        menuView.maskBackgroundOpacity = 0.3
+        menuView.didSelectItemAtIndexHandler = {(indexPath: Int) -> () in
+            // Setttings
+        }
+        
+        self.navigationItem.titleView = menuView
+    }
+
+}
+
+
+
