9a54b472f2668324b71beacdc6febc6d7834676f
suree404 <sureesuree404@gmail.com>
2016-04-15 21:05:01 -0500

    badge info

---
+++ Quit Smoking/Quit Smoking/BadgeViewTwo.swift
@@ -0,0 +1,30 @@
+//
+//  BadgeViewTwo.swift
+//  Quit Smoking
+//
+//  Created by Sushan Jiang on 4/15/16.
+//  Copyright © 2016 Le Tang Boon. All rights reserved.
+//
+
+import UIKit
+class BadgeViewTwo: ResponsiveTextFieldViewController{
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+}
